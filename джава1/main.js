5f31a8f1fa849f2f0e34979b807bddc44e958cc8
Тимур <151836256+TimurRival32@users.noreply.github.com>
2024-04-22 20:11:49 +0300

    Add files via upload

---
+++ джава1/main.js
@@ -0,0 +1,6 @@
+const lastName = 'Klyushenkov'
+const Name = 'Timur'
+const fathersName = 'Alexandrovich'
+const age = 16
+const myCity = 'Kovrov'
+console.log('Hello, I am ', lastName, Name, fathersName, ', my age is ', age, ', I am from ',myCity )
