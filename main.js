333b68e230d4de118075e72fa76f2693926b8c33
Тимур <151836256+TimurRival32@users.noreply.github.com>
2024-04-22 20:38:01 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const age = 16
-if(age>=18){
-    console.log('Вы совершеннолетний')
-}else{
-    console.log('Вы несовершеннолетний')
-}
-
-
-age >=18 ? console.log('Вы совершеннолетний') : console.log('Вы несовершеннолетний')
