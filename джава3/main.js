dbfbc9dfe1752b78a88a22ff66e199cf2de8a9c9
Тимур <151836256+TimurRival32@users.noreply.github.com>
2024-04-22 21:21:45 +0300

    Add files via upload

---
+++ джава3/main.js
@@ -0,0 +1,35 @@
+for(let i = 2; i<=12;i++){
+    if(i%2==0){
+        console.log(i)
+        i = i+1
+    }else{
+        i = i+1
+    }
+}
+
+console.log('///////////////////////////////')
+
+for(let x = 10; x!=0; x = x-1){
+    console.log(x)
+}
+
+console.log('///////////////////////////////')
+let j = 2
+
+while(j<=12){
+    if(j%2==0){
+        console.log(j)
+        j = j+1
+    }else{
+        j = j+1
+    }
+}
+
+console.log('///////////////////////////////')
+
+let h = 10
+
+while(h!=0){
+    console.log(h)
+    h = h-1
+}
