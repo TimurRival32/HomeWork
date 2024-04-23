493a2a0d11547ab9d74faf6e73fd4eace3b2355d
Тимур <151836256+TimurRival32@users.noreply.github.com>
2024-04-23 18:32:54 +0300

    Add files via upload

---
+++ джава81/main.js
@@ -0,0 +1,15 @@
+const massiveOne = [2,5,8,13,6,11,20]
+for(let i=0; i<massiveOne.length; i++){
+    if(massiveOne[i]%2==0){
+        console.log(massiveOne[i])
+    }
+}
+
+
+console.log('/////////////////////////////////////')
+
+const massiveTwo = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий', 'фиолетовый' ]
+for(let h=1; h-1!=massiveTwo.length; h++){
+    console.log(massiveTwo[massiveTwo.length-h])
+}
+
